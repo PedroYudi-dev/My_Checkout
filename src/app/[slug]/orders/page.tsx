d8e837cfc33a7c47326c0a20564798ad9bc21fd2
Pedro henrique yudi kawamoto <pedroyudi78@gmail.com>
2026-01-30 08:09:34 -0300

    fix: ensure user is redirectied with cpf after order creation

--- src/app/[slug]/orders/page.tsx
+++ src/app/[slug]/orders/page.tsx
@@ -18,6 +18,9 @@ const OrdersPage = async ({ searchParams }: IOrdersPageProps) => {
         return <CpfForms/>
     }
     const orders = await db.order.findMany({
+        orderBy:{
+            createdAt: "desc",
+        },
         where:{
             customerCpf: removeCpfPunctuation(cpf)
         },
